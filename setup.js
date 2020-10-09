3ee8ad3430de3f52d3531d33b1629ee7b7153fd2
= <=>
2020-10-09 16:59:52 +0700

    firebase_setup

---
+++ setup.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import firebase from '@react-native-firebase/app'
+import auth from '@react-native-firebase/auth'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDrfyv7f0UUjwj5MvrFBbbePEa2AkCQAEw",
+    authDomain: "fir-phoneauth-5599d.firebaseapp.com",
+    databaseURL: "https://fir-phoneauth-5599d.firebaseio.com",
+    projectId: "fir-phoneauth-5599d",
+    storageBucket: "fir-phoneauth-5599d.appspot.com",
+    messagingSenderId: "101590223426",
+    appId: "1:101590223426:web:c73347f4c43593c506bbdb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default () => {
+    return { firebase, auth }
+}
